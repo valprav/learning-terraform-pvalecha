d3cd1e970238b3544678af6a605c59726a44c66f
valprav <146423172+valprav@users.noreply.github.com>
2026-02-19 15:20:38 +0100

    learning to make changes to instances

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog.ami
+  value = aws_instance.shop.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.arn
-
+  value = aws_instance.shop.arn
+}
