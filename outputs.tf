8360e0138b517d7ba980a451ecf79a1acd66587f
valprav <146423172+valprav@users.noreply.github.com>
2026-02-19 15:51:30 +0100

    configure security groups

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.shop.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.shop.arn
+  value = aws_instance.blog.arn
 }
